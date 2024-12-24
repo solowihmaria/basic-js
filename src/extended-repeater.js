012c430870998278a035bc3a429e5b239be305e1
solowihmaria <solowihmaria@gmail.com>
2024-12-24 16:36:33 +0300

    feat: implement string repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str); 
+
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';  
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  
+  // сначала проще добавитть добавочную строку
+  const fullAddition = Array(additionRepeatTimes)
+    .fill(addition)
+    .join(additionSeparator);
+  
+  // формируем итоговую, объединяя с добавочной
+  const result = Array(repeatTimes)
+    .fill(str + fullAddition)
+    .join(separator);
+  
+  return result;
 }
 
 module.exports = {
