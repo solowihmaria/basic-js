946bea312fb3aa1e8fee1324f07b2120451ab317
solowihmaria <solowihmaria@gmail.com>
2024-12-25 14:48:01 +0300

    feat: implement MAC-48 address validation function

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(inputString) {
+  
+  const duos = inputString.split('-');
+  // пар символов должно быть 6
+  if (duos.length !== 6) {
+    return false;
+  }
+  // проверяем каждую пару
+  for (const duo of duos) {
+    // длина  должна быть ровно 2 символа
+    if (duo.length !== 2) {
+      return false;
+    }
+
+    // проверяем, что оба символа являются шестнадцатеричными числами
+    for (const char of duo) {
+      const isDigit = char >= '0' && char <= '9'; 
+      const isUpper = char >= 'A' && char <= 'F'; 
+      const isLower = char >= 'a' && char <= 'f'; 
+
+      if (!isDigit && !isUpper && !isLower) {
+        return false; 
+      }
+    }
+  }
+  return true; 
 }
 module.exports = {
   isMAC48Address
